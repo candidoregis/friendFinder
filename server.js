6a2126009d3d5b83f71f8de33bdb1790d2d6db5f
Candido Regis <candidorbb@gmail.com>
2019-08-22 11:04:06 -0400

    css to express fixed

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3090;
 
+// app.set("css", path.join(__dirname,"css"));
+
+
+app.use(express.static(path.join(__dirname, "app/public"), {maxAge: 3456700000}));
+// app.use(express.static(__dirname + "/public", {maxAge: 3456700000})); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 // require("./app/routing/apiRoutes.js")(app);
-require("./friendFinder/app/routing/htmlRoutes.js.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
