574c67ef1733c9ec80c1051f6457acfadab9c996
Candido Regis <regis.candido@intact.net>
2019-10-01 11:19:16 -0400

    Comments on server.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,18 @@
 var path = require("path");
+var friends = require("../data/friends.js");
 
 module.exports = function (app) {
     app.get("/data/friends", function (req, res) {
         return res.json(friends);
     });
 
-    app.get("/data/data/:friend", function (req, res) {
-        var chosen = req.params.friends;
+    app.post("/api/friends", function (req, res) {
 
-        console.log(chosen);
-
-        for (var i = 0; i < friends.length; i++) {
-            if (chosen === friends[i].routeName) {  //tem que rever routeName
-                return res.json(characters[i]);
-            }
+        var bestMatch = {
+            friendName: "",
+            friendPhoto: "",
+            friendsDistancePoints: 100
         }
 
-        return res.json(false);
-    });
-
-    app.post("/api/characters", function (req, res) {
-        var newCharacter = req.body;
-        newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-        console.log(newCharacter);
-        characters.push(newCharacter);
-        res.json(newCharacter);
     });
 };
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
+// DEPENDENCIES
 var express = require("express");
 var path = require("path");
 
+// EXPRESS CONFIG
 var app = express();
 var PORT = process.env.PORT || 3090;
 
-// app.set("css", path.join(__dirname,"css"));
-
-
+// MIDDLEWARE
 app.use(express.static(path.join(__dirname, "app/public"), {maxAge: 3456700000}));
-// app.use(express.static(__dirname + "/public", {maxAge: 3456700000})); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// require("./app/routing/apiRoutes.js")(app);
+// ROUTES
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
+// STARTING THE SERVER
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
