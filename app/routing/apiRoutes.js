35e1be15a558eb17d0461b70677abfde9f603812
Candido Regis <regis.candido@intact.net>
2019-10-02 16:30:09 -0400

    fixing apiRoutes errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,19 +18,22 @@ module.exports = function (app) {
 
         var userData = req.body;
         
-        var userScores = userData.scores;
+        var userScores = userData.userScores;
         //var userName = userData.name;
         //var userPhoto = userData.photo;
 
         console.log("------------------------------ USER DATA ");
         console.log(userData);
-
+        console.log("------------------------------ USER SCORE ");
+        console.log(userScores);
         var distancePoints = 0;
 
         for (var i = 0; i < friends.length-1; i++){
             distancePoints = 0;
             for(var j = 0; j < 10; j++){
-                distancePoints += Math.abs( userScores[j] - friends[i].scores[j]);
+                console.log("------------------------------ USER SCORE ");
+                console.log(userScores);
+                distancePoints += Math.abs( parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
                 console.log("------------------------------ DISTANCE POINTS ");
                 console.log(distancePoints);
             }
